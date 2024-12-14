7c222557e1ef1f27a6edc2d0de49ac196e3602da
Shepilova <magic.hollow@yandex.kz>
2024-12-14 14:17:31 +0400

    TЗ - 11. аннотация @Builder

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -14,6 +15,7 @@ import java.util.Set;
  * Film.
  */
 @Data
+@Builder
 public class Film {
     private long id = 0;
     @NotBlank(message = "Поле не должно быть пустым.")
@@ -25,5 +27,6 @@ public class Film {
     private LocalDate releaseDate;
     @Min(value = 1, message = "Длительность фильма не может быть меньше минуты.")
     private int duration;
+    @Builder.Default
     private Set<Long> likes = new HashSet<>();
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -12,6 +13,7 @@ import java.util.Set;
 
 
 @Data
+@Builder
 public class User {
     private long id = 0;
     @Email(message = "email введен некорректно.")
@@ -24,5 +26,6 @@ public class User {
     @Past(message = "Дата рождения не может быть в будущем.")
     @NotNull(message = "Поле не должно быть пустым.")
     private LocalDate birthday;
+    @Builder.Default
     private Set<Long> friends = new HashSet<>();
 }
